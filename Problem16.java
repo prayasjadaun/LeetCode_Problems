341f883c8487672e2588bac5ccecac0944da8fde
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-12-25 18:19:55 +0530

    Solved problem 16 is create target array

---
+++ Problem16.java
@@ -0,0 +1,45 @@
+/*
+ Create Target Array in the Given Order
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+
+It is guaranteed that the insertion operations will be valid.
+
+Example 1:
+
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+
+ */
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class Problem16 {
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> target = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            target.add(index[i], nums[i]);
+        }
+        return target.stream().mapToInt(i -> i).toArray();
+    }
+    public static void main(String[]args){
+        int [] nums = {0,1,2,3,4};
+        int [] index = {0,1,2,2,1};
+        int [] result = createTargetArray(nums, index);
+        for(int i : result){
+            System.out.print(i+",");
+        }
+    }
+}
