dac23816cbfa76fb771fcac5bde915c04f2de29c
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-04 13:30:33 +0530

    Solved Problem 27 is Maximum Product Difference Between Two Pairs

---
+++ Problem27.java
@@ -0,0 +1,44 @@
+/*
+ Maximum Product Difference Between Two Pairs
+The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+
+For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
+Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
+
+Return the maximum such product difference.
+Example 1:
+
+Input: nums = [5,6,2,7,4]
+Output: 34
+Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
+The product difference is (6 * 7) - (2 * 4) = 34.
+ */
+
+public class Problem27 {
+    public static int maxProductDifference(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+            } else if (nums[i] > max2) {
+                max2 = nums[i];
+            }
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+        return (max1 * max2) - (min1 * min2);
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 5, 6, 2, 7, 4 };
+        int result = maxProductDifference(nums);
+        System.out.println(result);
+    }
+}
